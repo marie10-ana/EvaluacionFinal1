b146e0b468bf806a5cd937cfcc2f2d351916764e
marie10-ana <marie10@umes.edu.gt>
2025-06-09 09:04:00 -0600

    Se crearon los listados y el formulario para ingresar los vehiculos

--- EvaluacionFinal1/Models/Automovil.cs
+++ EvaluacionFinal1/Models/Automovil.cs
@@ -1,11 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
 namespace EvaluacionFinal1.Models
-{
-    public class Automovil: Vehiculos
+{ public class Automovil: Vehiculos
     {
         public int NumPuertas { get; set; }
         [Required]
         public string TipoCombustible { get; set; }
     }
+   
 }

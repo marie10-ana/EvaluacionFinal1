ae92ede96fb4cd56a69e3f099c93ed39cb338c83
marie10-ana <marie10@umes.edu.gt>
2025-06-09 09:40:16 -0600

    Se tiene los vehiculos guardados en cada listado, y se creó el listado general

--- EvaluacionFinal1/Program.cs
+++ EvaluacionFinal1/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using EvaluacionFinal1;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorageAsSingleton();
 
 await builder.Build().RunAsync();
